f813dfc0c69cd3a50ac82855349d69bb142fc4fe
hila1597 <hilaryhidalgog@gmail.com>
2026-02-17 23:29:44 -0600

    Semana 1: proyecto de consola con Program.cs

--- Semana1App/Program.cs
+++ Semana1App/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Semana1App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tu nombre:");
+            string nombre = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Ingresa tu edad:");
+            int edad = int.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine($"Hola {nombre}, tienes {edad} años.");
+
+            int edadFutura = edad + 5;
+            Console.WriteLine($"En 5 años tendrás {edadFutura} años.");
+
+            if (edad >= 18)
+                Console.WriteLine("Eres mayor de edad.");
+            else
+                Console.WriteLine("Eres menor de edad.");
+        }
+    }
+}
